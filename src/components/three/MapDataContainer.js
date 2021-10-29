e335007afc9ced0e7b49586992bfe6f71b1bb548
Ash <zblash@outlook.com>
2021-10-29 11:37:27 +1100

    fix(*): Fix issues base on feedback

--- src/components/three/MapDataContainer.js
+++ src/components/three/MapDataContainer.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row} from "react-bootstrap";
+
+import referenceImg from './reference.png'
 import Map from "./Map";
 import DataVisualization from "./DataVisualization";
 import BarChart from "../charts/BarChart";
@@ -7,6 +9,10 @@ import AreaChartCustom from "../charts/AreaChartCustom";
 import PieChartCustom from "../charts/PieChartCustom";
 
 function MapContainer() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const [data, setData] = useState([]);
   const getData = () => {
     fetch("Final_data.json", {
@@ -63,7 +69,29 @@ function MapContainer() {
          <PieChartCustom></PieChartCustom>
         </Col>
       </Row>
+      <Row>
+          <Col xs={{span:2, offset:10}} style={{zIndex:10, fontWeight:500,fontStyle:'italic'}}> 
+          <p> (Data as of 24 October 2021)</p>
+          </Col>
+      </Row>
+      <Row>
+          <Col xs={{span:10,offset:1}} style={{zIndex:10}}>
+          <Button data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" style={{height:35+'px'}}> Reference and data source</Button>
+         
+          </Col>
+      </Row>
       </Card.Body>
+      
+
+      <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasBottomLabel">Offcanvas bottom</h5>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body small">
+          <img src={referenceImg}></img>
+        </div>
+      </div>
     </Card>
   );
 }
